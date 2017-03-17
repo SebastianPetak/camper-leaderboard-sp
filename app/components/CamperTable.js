ebe6fd037997d9afdbbe83527249fc4745b9e1df
Sebastian Petak <sebastianpetak@gmail.com>
2017-03-17 17:52:17 -0500

    Create CamperTable component and install axios Begin implimenting axios requests for the camper leaderboard data. Add CamperTable component as a child component of CamperLeaderboard.

---
+++ app/components/CamperTable.js
@@ -0,0 +1,14 @@
+const React = require('react');
+
+module.exports = class CamperTable extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return (
+			<div>
+				CamperTable is working!
+			</div>
+		);
+	}
+};
--- app/containers/CamperLeaderboard.js
+++ app/containers/CamperLeaderboard.js
@@ -1,13 +1,31 @@
 const React = require('react');
+import axios from 'axios';
+const CamperTable = require('../components/CamperTable');
 
 module.exports = class CamperLeaderboard extends React.Component {
 	constructor(props) {
 		super(props);
+		this.State = {
+			recentLeaderboard: {}
+		};
+
+		axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
+		.then(function(result) {
+			this.setState({
+				recentLeaderboard: result
+			});
+			//console.log(result);
+			//console.log(typeof(result));
+		})
+		.catch(function(error) {
+			console.log('This is my catch error: ' + error);
+		});
 	}
+
 	render() {
 		return (
 			<div>
-				Camper Leaderboard
+				<CamperTable />
 			</div>
 		);
 	}
